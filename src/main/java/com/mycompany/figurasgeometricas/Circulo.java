9058d5de48e93d3768d9624b10a538e955993e78
DavidRodriguez2552 <zorpresapecas@gmail.com>
2024-10-20 17:12:19 -0500

    esta clase es una clase derivada de FiguraGeometrica Sobrescribe los métodos para proporcionar area y perimetro de la figura circulo

---
+++ src/main/java/com/mycompany/figurasgeometricas/Circulo.java
@@ -0,0 +1,19 @@
+package com.mycompany.figurasgeometricas;
+
+public class Circulo extends FiguraGeometrica{
+
+    private double radio;
+
+    public Circulo(String nombre, String color, double radio) {
+        super(nombre, color);
+        this.radio = radio;
+    }
+
+    public double obtenerArea() {
+        return Math.PI*radio*radio ;
+    }
+
+    public double obtenerPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+}
