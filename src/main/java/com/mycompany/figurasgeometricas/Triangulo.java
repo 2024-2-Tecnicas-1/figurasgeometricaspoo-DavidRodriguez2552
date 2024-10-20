1dd2de3370b68e8737d593b0010947d98adf2f54
DavidRodriguez2552 <zorpresapecas@gmail.com>
2024-10-20 17:13:06 -0500

    esta clase es una clase derivada de FiguraGeometrica Sobrescribe los métodos para proporcionar area y perimetro de la figura triangulo

---
+++ src/main/java/com/mycompany/figurasgeometricas/Triangulo.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.figurasgeometricas;
+
+
+public class Triangulo extends FiguraGeometrica{
+    private double base;
+    private double altura;
+    
+    public Triangulo(String nombre, String color, double base, double altura){
+    super(nombre, color);
+    this.base = base;
+    this.altura = altura;
+    }
+    
+    public double obtenerArea(){
+    return (base*altura)/2;
+    }
+    
+    public double obtenerPerimetro(){
+    return base*3;
+    }
+}
